5307164c498a723a39cb6b27469eccb4d1ccda13
Miguel Caldas <miguelmeniciocaldas@gmail.com>
2026-04-09 09:17:28 +0100

    fix: update GET and POST request handling for improved response structure

--- express_class1/app.js
+++ express_class1/app.js
@@ -9,7 +9,23 @@ const port = process.env.PORT || 3000;
 
 // sets the server response to a GET request on URI /
 app.get("/", (req, res) => {
-  res.send("<html><body><h1>Hello World</h1></body></html>");
+  console.log("GET request received at /");
+  res.send("Hello World!");
+});
+
+app.get("/", (req, res) => {
+  console.log(" NEW GET request received at /");
+  res.json({ message: "Hello World!" });
+})
+
+app.post("/students", (req, res) => {
+  console.log("POST request received at /students");
+  res.json({ message: "Student created successfully!" });
+});
+
+app.all("/students", (req, res) => {
+  console.log(`Received ${req.method} request at /students`);
+  res.json({ message: `Handled ${req.method} request at /students` });
 });
 
 // server creation and listening for any incoming requests
